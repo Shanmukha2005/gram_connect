047965e5c769e73ae1fc1b51c2f5164851465fac
unknown <appajosyulashanmukha99@gmail.com>
2025-08-29 18:40:13 +0530

    Initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,8 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Media files (user uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -130,4 +134,3 @@ AUTH_USER_MODEL = 'members.Shopkeeper'
 import os
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
